6eda80ff4ffd076569c38078831a4259b0c9570d
Van Zheng <zheng_jinfan@126.com>
2023-05-19 17:44:45 +0800

    #领域事件demo

---
+++ Apps/CMS/CMS.AppService.FreeSql/DomainEventHandlers/TestEventHandler.cs
@@ -0,0 +1,32 @@
+using Athena.Infrastructure.Event.Interfaces;
+using CMS.Domain.Models.Events;
+
+namespace CMS.AppService.FreeSql.DomainEventHandlers;
+
+/// <summary>
+/// 测试领域事件
+/// </summary>
+public class TestEventHandler : AppServiceBase<Article>,
+    IDomainEventHandler<ArticlePublishedEvent>
+{
+    public TestEventHandler(UnitOfWorkManager unitOfWorkManager, ISecurityContextAccessor accessor) : base(
+        unitOfWorkManager, accessor)
+    {
+    }
+
+    /// <summary>
+    /// 文章发布成功事件
+    /// </summary>
+    /// <param name="notification"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public async Task Handle(ArticlePublishedEvent notification, CancellationToken cancellationToken)
+    {
+        var entity = await GetForUpdateAsync(notification.GetId()!, cancellationToken);
+        // 将标题改成45555+时间戳
+        entity.Title = "45555_" + DateTime.Now.Ticks;
+        entity.Author = "领域事件修改的";
+        await RegisterDirtyAsync(entity, cancellationToken);
+    }
+}
